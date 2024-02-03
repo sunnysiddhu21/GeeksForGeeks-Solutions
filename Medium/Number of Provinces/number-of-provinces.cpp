e63a99c785c0aeae427f9423272a23e583722f54
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2024-02-03 21:49:47 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of Provinces/number-of-provinces.cpp
+++ Medium/Number of Provinces/number-of-provinces.cpp
@@ -6,46 +6,75 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
-class Solution {
-  private:
-    void dfs(int node,vector<int>adj[],int vis[]){
-        vis[node]=1;
-        
-        for(auto it:adj[node]){
-            if(!vis[it]){
-                dfs(it,adj,vis);
-            }
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
         }
     }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+class Solution {
   public:
-    int numProvinces(vector<vector<int>> adj, int n) {
+    int numProvinces(vector<vector<int>> adj, int V) {
         // code here
-        
-        vector<int> adjLs[n];
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < n; j++)
-            {
-                if(adj[i][j] == 1)
-                {
-                    adjLs[i].push_back(j);
-                    adjLs[j].push_back(i);
+        DisjointSet ds(V);
+        for(int i=0; i<V; i++){
+            for(int j=0; j<V; j++){
+                if(adj[i][j]==1){
+                    ds.unionBySize(i,j);
                 }
             }
         }
         
-        int vis[n]={0};
         int cnt=0;
-
-        for(int i=0; i<n; i++){
-            if(!vis[i]){
-                cnt++;
-                dfs(i,adjLs,vis);
-            }
-        }
-
-        return cnt;
         
+        for(int i=0;i<V;i++){
+            if(ds.findUPar(i)==i) cnt++;
+        }
+        
+        return cnt;
     }
 };
 
