e5aa0f762849f3946aad635fe68920ac2a44e2c6
Sunny Siddhu <119279854+sunnysiddhu21@users.noreply.github.com>
2024-02-03 15:00:49 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Anagram/anagram.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function is to check whether two strings are anagram of each other or not.
+    bool isAnagram(string s, string t){
+        
+        // Your code here
+        if(s.length()!=t.length()) return false;
+        int count[26] = {0};
+        
+        // Count the frequency of characters in string s
+        for (char x : s) {
+            count[x - 'a']++;
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (char x : t) {
+            count[x - 'a']--;
+        }
+        
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    
+    int t;
+
+    cin >> t;
+
+    while(t--){
+        string c, d;
+
+        cin >> c >> d;
+        Solution obj;
+        if(obj.isAnagram(c, d)) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+
+}
+
+// } Driver Code Ends
